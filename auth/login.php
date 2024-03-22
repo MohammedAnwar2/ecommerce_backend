c149e96e878a38169f374f169c988aa5acc69ee6
mohammed anwar <moanbm123@gmail.com>
2024-03-22 19:15:41 +0530

    Add user approval check in login query
    Add user approval check in the login query to only allow login for approved users.

--- auth/login.php
+++ auth/login.php
@@ -5,7 +5,7 @@ include "../connect.php";
 $password = sha1($_POST['password']);
 $email = filterRequest('email');
 
-$statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ? AND `users_password` = ? ");
+$statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ? AND `users_password` = ? `users_approve`=1");
 $statment->execute([$email,$password]);
 $count = $statment->rowCount();
 if($count>0){
