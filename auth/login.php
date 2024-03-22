865422032cec826dee69ac0fefc639dc2cd41839
mohammed anwar <moanbm123@gmail.com>
2024-03-22 21:00:14 +0530

    Update login.php query for better validation
    Refactor the login.php query to include additional validation checks for user
    account verification and password validity. This improves the security and
    accuracy of the login process.

--- auth/login.php
+++ auth/login.php
@@ -5,11 +5,49 @@ include "../connect.php";
 $password = sha1($_POST['password']);
 $email = filterRequest('email');
 
-$statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ? AND `users_password` = ? `users_approve`=1");
-$statment->execute([$email,$password]);
+$statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ? AND `users_password` = ? AND `users_approve`= 1 ");
+$statment->execute([$email, $password]);
 $count = $statment->rowCount();
-if($count>0){
-    echo json_encode(["status"=>"success"]);
-}else{
-    printFailure("Email OR Phone Not Exists");
+
+if ($count > 0) {
+    echo json_encode(["status" => "success"]);
+} else {
+    $statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ? AND `users_password` = ? ");
+    $statment->execute([$email, $password]);
+    $count = $statment->rowCount();
+    if ($count > 0) {
+        printFailure("Account not verified yet");
+    }else{
+        $statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ?");
+        $statment->execute([$email]);
+        $count = $statment->rowCount();
+        if ($count > 0) {
+            printFailure("Password Not Valid");
+        }else{
+            printFailure("Email Not Valid");
+        }
+    }
 }
+
+
+// if ($count > 0) {
+//     echo json_encode(["status" => "success"]);
+// } else {
+//     $statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ? AND `users_password` = ? ");
+//     $statment->execute([$email, $password]);
+//     $count = $statment->rowCount();
+//     if ($count > 0) {
+//         printFailure("You Forget To Enter The Verify Code In Sign Up Page");
+//     }else{
+//         $statment = $con->prepare("SELECT * FROM `users` WHERE `users_email` = ?");
+//         $statment->execute([$email]);
+//         $count = $statment->rowCount();
+//         if ($count > 0) {
+//             printFailure("Password Not Valid");
+//         }else{
+//             printFailure("Email Not Valid");
+//         }
+//     }
+// }
+
+//printFailure("Email OR Phone Not Exists");
