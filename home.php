9ba9bcd2ef096548d00fac4fcddc97888b8612f4
mohammed anwar <moanbm123@gmail.com>
2024-04-05 15:15:07 +0530

    Add new SVG files for various items
    Added new SVG files for different items such as laptop, mobile, dress, headphones, t-shirt, wallet, water bottle, sports bottle, and laptop bag.

--- home.php
+++ home.php
@@ -6,6 +6,8 @@ $allData['categories'] = getAllData("categories",null,null,false);
 if(count($allData['categories'])==0)
 {
     $allData['status']="failure";
+}else{
+    $allData['items'] = getAllData("itemview",null,null,false);
 }
 echo json_encode($allData);
 
