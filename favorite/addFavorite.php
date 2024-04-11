0f1647b95ff3abf9e5f66473f53b9e32eba3a535
mohammed anwar <moanbm123@gmail.com>
2024-04-12 01:32:37 +0530

    Add favorite and remove favorite functionality
    Add PHP scripts to add and remove items from user's favorites list. Include
    necessary database operations to insert and delete data accordingly.

---
+++ favorite/addFavorite.php
@@ -0,0 +1,10 @@
+<?php
+include "../connect.php";
+$usersId = filterRequest("usersId");
+$itemsId = filterRequest("itemsId");
+$data = array(
+    "favorite_usersId" => $usersId,
+    "favorite_itemsId" => $itemsId
+);
+insertData("favorite", $data);
+?>
