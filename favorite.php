3ed3fc9c27fe1a59a8a54ec3f3f21c10a42be79b
mohammed anwar <moanbm123@gmail.com>
2024-04-13 01:20:41 +0530

    Remove HTML comment tags from favorite.php
    Remove unnecessary HTML comment tags from favorite.php to improve code readability
    and maintain consistency.

--- favorite.php
+++ favorite.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 include "connect.php";
 $userId = filterRequest('userId');
 $itemId = filterRequest('itemId');
@@ -12,7 +12,7 @@ if (count($allData['favoriteItem']) == 0) {
     insertData("favorite", $data);
 } else {
     deleteData("favorite", "favorite_usersId = $userId AND favorite_itemsId = $itemId");
-} -->
+}  
 
 
 // $allData['favoriteItem'] = getAllData("favoriteItem", "favorite_usersId = ? AND favorite_itemsId = ?", [$userId, $itemId], false);
