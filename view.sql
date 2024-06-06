d1307ede935a7ec46663d59cc005cc2e378476e8
mohammed anwar <moanbm123@gmail.com>
2024-06-06 08:52:34 +0530

    create orderview and orderdetailsview orderview -> to get address of all the users order orderdetailsview -> to get details of all the products that ordered

--- view.sql
+++ view.sql
@@ -10,7 +10,7 @@ FROM items JOIN cart
 ON items.items_id = cart.cart_itemsId JOIN users
 ON users.users_id = cart_usersId
 WHERE cart_orders = 0
-GROUP BY items.items_price ,cart.cart_usersId ;
+GROUP BY items.items_price ,cart.cart_usersId , cart.cart_orders ;
 -- CREATE OR REPLACE VIEW cartProducts AS
 -- SELECT ROUND(SUM(items.items_price - (items.items_price*items.items_discount/100)), 2) AS total_price , COUNT(items.items_price) as Itemscount, cart.*,items.*
 -- FROM items JOIN cart
@@ -23,3 +23,23 @@ GROUP BY items.items_price ,cart.cart_usersId ;
 CREATE OR REPLACE VIEW favoriteSearch AS
 SELECT DISTINCT itemview.* FROM itemview JOIN favorite
 ON itemview.items_id = favorite.favorite_itemsId;
+
+
+--* we using LEFT JOIN to get everything , the address is there or not , we will get everything
+--! this qury to get the address
+CREATE OR REPLACE VIEW ordersview AS
+SELECT orders.* , address.*
+FROM orders 
+LEFT JOIN address 
+ON orders.orders_addressId = address.address_id
+
+
+
+CREATE OR REPLACE VIEW orderdetailsview AS
+SELECT ROUND(SUM(items.items_price - (items.items_price*items.items_discount/100)), 2) AS total_price , COUNT(items.items_price) as Itemscount, cart.*,items.* ,ordersview.*
+FROM orders, items 
+JOIN cart ON items.items_id = cart.cart_itemsId 
+JOIN ordersview ON ordersview.orders_id = cart.cart_orders 
+JOIN users ON users.users_id = cart_usersId
+WHERE cart_orders != 0 AND orders.orders_id = cart.cart_orders
+GROUP BY items.items_price ,cart.cart_usersId , cart.cart_orders ;
